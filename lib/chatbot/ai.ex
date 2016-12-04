a791e16e1f7bdb8fbc5c8dff67c2a81d8813f4aa
Alice <amyalicedee@gmail.com>
2016-12-04 10:35:04 +0000

    Using cond and regexes not pattern matching

--- lib/chatbot/ai.ex
+++ lib/chatbot/ai.ex
@@ -1,14 +1,22 @@
 #nb couuld be rewritten just using cases
 defmodule Chatbot.Ai do
+
+  def interpret(text) do
+    cond do
+      String.match?(text, ~r/hi|hello|hola/i) -> "hello there"
+      String.match?(text, ~r/how do you do/i) -> "Great, and you?"
+    end
+  end
+
   # def interpret(text) do
-  def interpret("hi") do
-    "oh hello there"
-  end
-  def interpret("shut up") do
-    "you shut up"
-  end
-  # this one has to go last because basically we are doing case pattern matching on the input text with te interpret function 
-  def interpret(_foo) do
-    "don't tell me to #{_foo}"
-  end
+  # def interpret("hi") do
+  #   "oh hello there"
+  # end
+  # def interpret("shut up") do
+  #   "you shut up"
+  # end
+  # # this one has to go last because basically we are doing case pattern matching on the input text with te interpret function 
+  # def interpret(_foo) do
+  #   "don't tell me to #{_foo}"
+  # end
 end
--- lib/chatbot/cli.ex
+++ lib/chatbot/cli.ex
@@ -1,17 +1,18 @@
 defmodule Chatbot.Cli do
-
+  # these are just things you can do (might be broken in mine)
+  # import String, only: [trim: 1, downcase: 1]
+  # alias Chatbot.Ai, as: MyAi
   def run do
     loop()
   end
 
   defp loop do
+    IO.gets("> ")
+    |> String.trim
+    |> String.downcase
+    |> Chatbot.Ai.interpret()
+    |> IO.puts()
 
-  IO.gets("> ")
-  |> String.trim
-  |> String.downcase
-  |> Chatbot.Ai.interpret()
-  |> IO.puts()
-
-     loop()
+    loop()
   end
 end
